fbd08b7e59ac52669f8a2d60b8fffc1aa0015f7d
MercyL1n <18506000485@163.com>
2020-09-04 19:20:03 +0800

    first commit

---
+++ src/renderer/components/Browser/Browser.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class="title">任务窗口</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .title {
+    color: #2c3e50;
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 6px;
+  }
+</style>
---
+++ src/renderer/components/TargetList/TargetList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="title">靶机列表</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .title {
+    color: #2c3e50;
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 6px;
+  }
+</style>
