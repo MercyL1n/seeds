fb6af30e53e4227cba733213157d84ce728998e1
h0lyDuck <46277346+h0lyDuck@users.noreply.github.com>
2020-09-05 20:22:19 +0800

    丑的不能再丑的文件查看
    丑的不能再丑的文件查看

--- src/renderer/components/TargetList/TargetList.vue
+++ src/renderer/components/TargetList/TargetList.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="title">靶机列表</div>
+  <div class="title">靶机列表
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+  }
 </script>
 
 <style>
-  .title {
-    color: #2c3e50;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
+.title {
+  color: #2c3e50;
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 6px;
+}
 </style>
